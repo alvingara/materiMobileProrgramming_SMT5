b65f2b5f0777acab638c813f80c153fccd712d8f
ahmadKhairul4r <DekuCan24@gmail.com>
2024-09-23 18:55:14 +0700

    Tes

---
+++ src/app/contact/page.js
@@ -0,0 +1,45 @@
+export default function Contact(){
+    return (
+        <>
+            <div>
+                <h2 className="text-center text-3xl w-full py-4 font-bold">Get In Touch</h2>
+                <p className="text-center mx-auto w-2/6     ">
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                </p>
+            </div>
+
+            <div className="flex mt-16">
+                <div className="md:w-3/4 px-4">
+                <div className="bg-white p-10 rounded-x1">
+                    <h3 className="text-2x1 py-2">Leave a message</h3>
+                    <div className="flex gap-6">
+                        <div className="w-full">
+                            <label>Name</label>
+                            <input type="text" className="border my-input-text w-full"></input>
+                        </div>
+                        <div className="w-full">
+                            <label>Email</label>
+                            <input type="text" className="border my-input-text w-full"></input>
+                        </div>
+                        </div>
+                        <div className="w-full my-2">
+                            <label>Subject</label>
+                            <input type="text" className="my-input-text w-full"></input>
+                        </div>
+                        <div className="w-full">
+                            <label>Message</label>
+                            <textarea className="border my-input-text w-full"></textarea>
+                        </div>
+                        <div className="w-full py-2">
+                            <button className="mx-1 h-9 items-center justify-center px-4 rounded-md bg-amber-500">
+                                <label>Send Message</label>
+                            </button>
+                        </div>
+                    </div>
+                
+            </div>
+                <div className="md:w-1/3 border">kanan</div>
+            </div>
+        </>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+//Konfigurasi Talwind itu sendiri
+//fungsi talwind untuk class2 yg disediakan, yg membedakan talwind dan css hanya ada di dalam class nya aja
+//gap jarak antara grid2 lainnya
